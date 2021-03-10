f564db8c84335966282e9779aa0b4a77dd4e458d
halil <halil@DESKTOP-38P5TGT>
2021-03-11 02:04:58 +0300

    Day 11

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 23)
+            if (DateTime.Now.Hour == 02)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintanceTime);
             }
